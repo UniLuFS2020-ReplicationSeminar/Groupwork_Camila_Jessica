7d44056f6ef7bb74841fa73a14a08f1d7328b98e
jessicaugowe <ugowejessicajapheth@gmail.com>
2024-03-26 11:02:47 +0100

    Loading the framingham data.

---
+++ 01_import.R
@@ -0,0 +1,8 @@
+#loading data
+
+framingham_data <- read.csv("C:/Users/Jessica J. Ugowe/Documents/FRÜHLING 2024/Data Mining/framingham.csv")
+
+
+View(framingham_data)
+str(framingham_data)
+head(framingham_data)
