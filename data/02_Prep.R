ae5ba78c3152146f8715e920f9619dce9a322281
jessicaugowe <ugowejessicajapheth@gmail.com>
2024-03-26 11:48:13 +0100

    changed the column name because it was too long and confusing

--- data/02_Prep.R
+++ data/02_Prep.R
@@ -2,3 +2,5 @@
 # Remove rows with "NA" values
 cleaned_data <- na.omit(framingham_data)
 
+# Assuming 'df' is your data frame
+colnames(framingham_data)[colnames(framingham_data) == "prevalentHyp"] <- "pHyp"
