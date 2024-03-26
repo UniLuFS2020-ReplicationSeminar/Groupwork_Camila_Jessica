105b7f2145284e4f6554e66c58b7db4791ca10a7
jessicaugowe <ugowejessicajapheth@gmail.com>
2024-03-27 00:49:01 +0100

    performed t-test

--- 04_Analysis.R
+++ 04_Analysis.R
@@ -4,3 +4,11 @@
 logistic_model <- glm(pHyp ~ BMI, data = cleaned_data, family = binomial)
 
 logistic_model
+
+# data for hypertensive and non-hypertensive groups
+pHyp_bmi <- cleaned_data$BMI[cleaned_data$pHyp == 1]
+nopHyp_bmi <- cleaned_data$BMI[cleaned_data$pHyp == 0]
+
+# Performing t-test
+t_test_result <- t.test(pHyp_bmi, nopHyp_bmi)
+print(t_test_result)
