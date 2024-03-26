eaf56e06d2c6a81036eff47e6cc8c26ccd9d9f95
jessicaugowe <ugowejessicajapheth@gmail.com>
2024-03-26 11:58:05 +0100

    changed the position of the written code to change the column name for prevelentHyp

--- data/02_Prep.R
+++ data/02_Prep.R
@@ -1,6 +1,5 @@
+colnames(framingham_data)[colnames(framingham_data) == "prevalentHyp"] <- "pHyp"
 
 # Remove rows with "NA" values
 cleaned_data <- na.omit(framingham_data)
 
-# Assuming 'df' is your data frame
-colnames(framingham_data)[colnames(framingham_data) == "prevalentHyp"] <- "pHyp"
